3864d25034bc5ebdad3038d5acf08d6f3640a46b
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2023-06-06 16:52:37 +0300

    First runnable application

---
+++ src/main/java/com/vburcovschi/spring/springboot/spring_boot/dao/EmployeeDAOImpl.java
@@ -0,0 +1,41 @@
+package com.vburcovschi.spring.springboot.spring_boot.dao;
+
+import com.vburcovschi.spring.springboot.spring_boot.entity.Employee;
+import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+    @Autowired
+    private EntityManager entityManager;
+    @Override
+    public List<Employee> getAllEmployee() {
+        Session session = entityManager.unwrap(Session.class);
+        return session.createQuery("from Employee", Employee.class).getResultList();
+    }
+
+/*    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = entityManager.unwrap(Session.class);
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = entityManager.unwrap(Session.class);
+        return session.get(Employee.class,id);
+    }
+
+    @Override
+    public void removeEmployee(int id) {
+        Session session = entityManager.unwrap(Session.class);
+        session.remove(getEmployee(id));
+    }*/
+
+
+}
